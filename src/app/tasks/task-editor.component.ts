85b0b6bed7a03b86ca3c50b926e4e5bd7e150814
Derek Lin <Derek Lin>
2016-07-28 19:53:16 -0700

    change cancel to use route

--- src/app/tasks/task-editor.component.ts
+++ src/app/tasks/task-editor.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'pomodoro-tasks-editor',
-
-  templateUrl: 'task-editor.component.html'
+  templateUrl: 'task-editor.component.html',
+  directives: [ROUTER_DIRECTIVES]
 })
 export default class TaskEditorComponent {
   constructor() {}
